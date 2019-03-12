d9162437f91551aec815d7cd32f6d4f47c47c407
JPDom1 <47320002+JPDom1@users.noreply.github.com>
2019-03-12 13:31:34 +0000

    Free disk space

---
+++ Server Administration - Free disk space.ps1
@@ -0,0 +1,8 @@
+﻿#Check local or Remote computer free disk space
+#This is only for the C drive to add other drives change the drive letter
+#JP Erasmus
+#12/03/2019
+#USE AT OWN RISK!
+$server = Read-Host -Prompt 'Input your Server Name'
+$disk = ([wmi]"\\$server\root\cimv2:Win32_logicalDisk.DeviceID='c:'")
+"$server C: has {0:#.0} GB free of {1:#.0} GB Total" -f ($disk.FreeSpace/1GB),($disk.Size/1GB) | write-output
