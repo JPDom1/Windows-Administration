a2b49fa4c36ad4922aa034f4ee49715b3428120b
JP Erasmus <jean-pierre.erasmus@outlook.com>
2019-03-29 10:59:09 +0000

    Create Ping Computers from list for up or down status in green or red.ps1
    New Script Ping computers for UP or Down status from list.

---
+++ Ping Computers from list for up or down status in green or red.ps1
@@ -0,0 +1,21 @@
+﻿<#
+Ping Computers for Up or Down status in green or red.
+Provide a TXT file with list of hostnames to ping
+JP Erasmus
+29/03/2019
+USE AT OWN RISK!
+#>
+
+function Get-Status {
+ 
+    Get-Content "C:\path\to\computerlisttxt" | 
+        foreach {
+            if (-not (Test-Connection -ComputerName $_ -quiet -Count 1)){
+                Write-host "$_ is down" -ForegroundColor Red
+            } Else {
+                    write-host "$_  is UP" -ForegroundColor Green
+                    }
+            }
+        }
+ 
+Get-Status
